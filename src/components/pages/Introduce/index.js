d9a621b7e8ba52de1592703331af5281260b3979
hong9Lee <hongtemp9mac@gmail.com>
2022-01-29 16:09:28 +0900

    소개 - 기술스택 contents 추가

--- src/components/pages/Introduce/index.js
+++ src/components/pages/Introduce/index.js
@@ -6,7 +6,7 @@ const IntroMiddle = () => {
         <main className="main" style={{clear : "both"}}>
             <PageNav text="소개" href="/introduce"/>
 
-            <section className="title_section">
+            <section className="intro_title_section">
                 <div>
                     <h2>
                         반갑습니다!
@@ -21,79 +21,340 @@ const IntroMiddle = () => {
                 <ul className="intro_box">
                     <li>
                         <div className="intro">
+
                             <div className="intro_img">
-                                <img src="/images/honggyulee.jpeg" alt="intro img"/>
+                                <img src="/images/introduce/honggyulee.jpeg" alt="intro img"/>
                             </div>
+
                             <div className="box_list">
                                 <div className="int name">
-                                    <div>
-                                        <img src="/images/me.png" alt="me img"/>
+                                    <div className="img_div">
+                                        <img src="/images/introduce/me.png" alt="me img"/>
                                     </div>
 
                                     <div className="contents">
                                         <h2>이름</h2>
                                         <p>이홍규</p>
                                     </div>
-
                                 </div>
 
                                 <div className="int name">
-                                    <div>
-                                        <img src="/images/me.png" alt="me img"/>
+                                    <div className="img_div">
+                                        <img src="/images/introduce/calender.png" alt="me img"/>
                                     </div>
 
                                     <div className="contents">
                                         <h2>생년월일</h2>
                                         <p>91.11.21</p>
                                     </div>
-
                                 </div>
 
                                 <div className="int name">
-                                    <div>
-                                        <img src="/images/me.png" alt="me img"/>
+                                    <div className="img_div">
+                                        <img src="/images/introduce/location.png" alt="me img"/>
                                     </div>
 
                                     <div className="contents">
                                         <h2>주소</h2>
                                         <p>경기도 군포시</p>
                                     </div>
-
                                 </div>
 
 
                                 <div className="int name">
-                                    <div>
-                                        <img src="/images/me.png" alt="me img"/>
+                                    <div className="img_div">
+                                        <img src="/images/introduce/phone.png" alt="me img"/>
                                     </div>
 
                                     <div className="contents">
                                         <h2>연락처</h2>
                                         <p>010-2749-5136</p>
                                     </div>
-
                                 </div>
 
                                 <div className="int name">
-                                    <div>
-                                        <img src="/images/me.png" alt="me img"/>
+                                    <div className="img_div">
+                                        <img src="/images/introduce/grad.png" alt="me img"/>
                                     </div>
 
                                     <div className="contents">
                                         <h2>학력</h2>
                                         <p>한국산업기술대학교 소프트웨어공학과</p>
                                     </div>
-
                                 </div>
-
                             </div>
                         </div>
-
-
                     </li>
                 </ul>
                 </div>
             </section>
+
+            <section className="title_section">
+                <div>
+                    <h2>
+                        기술 스택 소개
+                    </h2>
+                    <br/>
+                    <h3>
+                        보유하고있는 기술들을 나열했습니다.
+                        <br/>
+                        현업에서 경험하지 못한 기술들은 토이 프로젝트를 통해 습득했습니다.
+                    </h3>
+                </div>
+            </section>
+
+            <section className="skills_section">
+                <div>
+
+                    <div className="fx contents_div">
+                        <div className="skills_intro_div">
+                            <img src="/images/introduce/calender.png" alt="me img"/>
+                            <div>
+                                <h3>Frontend</h3>
+                                <p>퍼블리싱 & 프레임워크</p>
+                            </div>
+                        </div>
+
+                        <div className="skills_contents_div">
+                            <ul>
+                                <li>
+                                    <img src="/images/introduce/calender.png" alt="me img"/>
+                                    <div>
+                                        <h2>
+                                            HTML
+                                            <span>실무</span>
+                                        </h2>
+                                        <span>태그를 활용한 마크업</span>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <img src="/images/introduce/calender.png" alt="me img"/>
+                                    <div>
+                                        <h2>
+                                            HTML
+                                            <span>실무</span>
+                                        </h2>
+                                        <span>태그를 활용한 마크업</span>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <img src="/images/introduce/calender.png" alt="me img"/>
+                                    <div>
+                                        <h2>
+                                            HTML
+                                            <span>실무</span>
+                                        </h2>
+                                        <span>태그를 활용한 마크업</span>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <img src="/images/introduce/calender.png" alt="me img"/>
+                                    <div>
+                                        <h2>
+                                            HTML
+                                            <span>실무</span>
+                                        </h2>
+                                        <span>태그를 활용한 마크업</span>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <img src="/images/introduce/calender.png" alt="me img"/>
+                                    <div>
+                                        <h2>
+                                            HTML
+                                            <span>실무</span>
+                                        </h2>
+                                        <span>태그를 활용한 마크업</span>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <img src="/images/introduce/calender.png" alt="me img"/>
+                                    <div>
+                                        <h2>
+                                            HTML
+                                            <span>실무</span>
+                                        </h2>
+                                        <span>태그를 활용한 마크업</span>
+                                    </div>
+                                </li>
+
+                            </ul>
+                        </div>
+
+                    </div>
+
+                    <div className="fx contents_div">
+                        <div className="skills_intro_div">
+                            <img src="/images/introduce/calender.png" alt="me img"/>
+                            <div>
+                                <h3>Frontend</h3>
+                                <p>퍼블리싱 & 프레임워크</p>
+                            </div>
+                        </div>
+
+                        <div className="skills_contents_div">
+                            <ul>
+                                <li>
+                                    <img src="/images/introduce/calender.png" alt="me img"/>
+                                    <div>
+                                        <h2>
+                                            HTML
+                                            <span>실무</span>
+                                        </h2>
+                                        <span>태그를 활용한 마크업</span>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <img src="/images/introduce/calender.png" alt="me img"/>
+                                    <div>
+                                        <h2>
+                                            HTML
+                                            <span>실무</span>
+                                        </h2>
+                                        <span>태그를 활용한 마크업</span>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <img src="/images/introduce/calender.png" alt="me img"/>
+                                    <div>
+                                        <h2>
+                                            HTML
+                                            <span>실무</span>
+                                        </h2>
+                                        <span>태그를 활용한 마크업</span>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <img src="/images/introduce/calender.png" alt="me img"/>
+                                    <div>
+                                        <h2>
+                                            HTML
+                                            <span>실무</span>
+                                        </h2>
+                                        <span>태그를 활용한 마크업</span>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <img src="/images/introduce/calender.png" alt="me img"/>
+                                    <div>
+                                        <h2>
+                                            HTML
+                                            <span>실무</span>
+                                        </h2>
+                                        <span>태그를 활용한 마크업</span>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <img src="/images/introduce/calender.png" alt="me img"/>
+                                    <div>
+                                        <h2>
+                                            HTML
+                                            <span>실무</span>
+                                        </h2>
+                                        <span>태그를 활용한 마크업</span>
+                                    </div>
+                                </li>
+
+                            </ul>
+                        </div>
+
+                    </div>
+
+                    <div className="fx contents_div">
+                        <div className="skills_intro_div">
+                            <img src="/images/introduce/calender.png" alt="me img"/>
+                            <div>
+                                <h3>Frontend</h3>
+                                <p>퍼블리싱 & 프레임워크</p>
+                            </div>
+                        </div>
+
+                        <div className="skills_contents_div">
+                            <ul>
+                                <li>
+                                    <img src="/images/introduce/calender.png" alt="me img"/>
+                                    <div>
+                                        <h2>
+                                            HTML
+                                            <span>실무</span>
+                                        </h2>
+                                        <span>태그를 활용한 마크업</span>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <img src="/images/introduce/calender.png" alt="me img"/>
+                                    <div>
+                                        <h2>
+                                            HTML
+                                            <span>실무</span>
+                                        </h2>
+                                        <span>태그를 활용한 마크업</span>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <img src="/images/introduce/calender.png" alt="me img"/>
+                                    <div>
+                                        <h2>
+                                            HTML
+                                            <span>실무</span>
+                                        </h2>
+                                        <span>태그를 활용한 마크업</span>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <img src="/images/introduce/calender.png" alt="me img"/>
+                                    <div>
+                                        <h2>
+                                            HTML
+                                            <span>실무</span>
+                                        </h2>
+                                        <span>태그를 활용한 마크업</span>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <img src="/images/introduce/calender.png" alt="me img"/>
+                                    <div>
+                                        <h2>
+                                            HTML
+                                            <span>실무</span>
+                                        </h2>
+                                        <span>태그를 활용한 마크업</span>
+                                    </div>
+                                </li>
+
+                                <li>
+                                    <img src="/images/introduce/calender.png" alt="me img"/>
+                                    <div>
+                                        <h2>
+                                            HTML
+                                            <span>실무</span>
+                                        </h2>
+                                        <span>태그를 활용한 마크업</span>
+                                    </div>
+                                </li>
+
+                            </ul>
+                        </div>
+
+                    </div>
+
+                </div>
+            </section>
+
 
         </main>
     </>);
