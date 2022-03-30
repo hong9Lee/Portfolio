e723572d0a681abc1f76d94208a1a5c068501614
hong9Lee <hongtemp9mac@gmail.com>
2022-03-30 22:32:07 +0900

    nice 문구 수정

--- src/components/pages/Project/nice.js
+++ src/components/pages/Project/nice.js
@@ -18,7 +18,7 @@ const PageNice = () => {
             <section className="outline">
                 <div className="u_line">
                     <h2>프로젝트 개요</h2>
-                    <p>과거에 사람이 직접 손으로 기술하던 기업에 대한 의견을 자동화 시스템으로 구축 하는 사업임.</p>
+                    <p>과거에 사람이 직접 손으로 기술하던 기업에 대한 의견을 자동화 시스템으로 구축 하는 프로젝트</p>
 
                     <img className="outline_1" src="/images/project/nice/outline_1.png" alt="나이스 프로젝트 개요"/>
 
@@ -26,13 +26,13 @@ const PageNice = () => {
                     <br/>
                     <h2>1. 설계</h2>
                     <p>OOP를 활용하여 필요한 데이터를 파라미터로 입력받아 리소스 파일에 설정된 값에 의해 <br/>
-                        요청한 의견에 대한 완성문장을 반환하는 솔루션을 설계함.</p>
+                        요청한 의견에 대한 완성문장을 반환하는 솔루션을 설계</p>
 
 
                     <br/>
                     <br/>
                     <h2>2. 데이터 색인</h2>
-                    <p>약 1억 3천만건의 원천DB를 Logstash를 이용해 ElasticSearch에 수집함.</p>
+                    <p>약 1억 3천만건의 원천DB를 Logstash를 이용해 ElasticSearch에 수집</p>
 
 
                     <br/>
@@ -56,16 +56,16 @@ const PageNice = () => {
                     <h2>4. RULE 작성</h2>
                     <p>의견의 조건 분기를 위해 리소스 파일을 작성함. (안정적인 한글 지원을 위해 Groovy를 사용함)</p>
                     <br/>
-                    <p>Groovy Script : 의견생성을 위한 유형별 조건식.</p>
+                    <p>Groovy Script : 의견생성을 위한 유형별 조건식</p>
                     <img className="scr_1" src="/images/project/nice/groovy_script.png" alt="의견 1"/>
                     <br/>
                     <br/>
 
-                    <p>표현식 Script : 의견생성을 위한 유형별 표현식. 가중치와 문장패턴에 따라 다양한 문장 생성.</p>
+                    <p>표현식 Script : 의견생성을 위한 유형별 표현식. 가중치와 문장패턴에 따라 다양한 문장 생성</p>
                     <img className="scr_2" src="/images/project/nice/exp_script.png" alt="의견 1"/>
                     <br/>
                     <p>library 내부에서 데이터를 조회해 Groovy Script의 변수에 해당하는 값 <span className="ex">ex) 관계사_개수</span> 를 찾아내고,</p>
-                    <p>그 값에 해당하는 표현식을 표현식 Script 에서 찾아 문장을 만들어 return 함.</p>
+                    <p>그 값에 해당하는 표현식을 표현식 Script 에서 찾아 문장을 만들어 return</p>
 
 
                     <br/>
@@ -73,14 +73,14 @@ const PageNice = () => {
                     <br/>
                     <br/>
                     <h2>5. 단위, 성능 테스트</h2>
-                    <p>JUnit을 이용해 메서드 단위로 테스트를 진행, JMeter를 이용해 솔루션 성능 테스트를 진행함.</p>
+                    <p>JUnit을 이용해 메서드 단위로 테스트를 진행, JMeter를 이용해 솔루션 성능 테스트를 진행</p>
                     <img className="opn_jmeter" src="/images/project/nice/jmeter.png" alt="jmeter 테스트"/>
 
                     <br/>
                     <br/>
                     <br/>
                     <h2>6. 배포</h2>
-                    <p>Jar 패키징하여 배포함.</p>
+                    <p>Jar 패키징하여 배포</p>
 
                     <br/>
                     <br/>
